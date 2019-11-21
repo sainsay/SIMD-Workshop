648e5eef994d6c7cc036705e636562e8247682c8
Jens Hagen <hagen-j@outlook.com>
2019-11-21 10:12:15 +0100

    configured cmake and added dummy files

--- install.bat
+++ install.bat
@@ -11,15 +11,26 @@ echo ==== SIMD Workshop ====
 echo =======================
 
 echo ==== Generating Project Files ====
-if exist "./build_vs2017_win64/" (
-  del ".\build_vs2017_win64\CMakeCache.txt"
+if exist "./vsproject/" (
+  del ".\vsproject\CMakeCache.txt"
 )
 
-mkdir build_vs2017_win64
-cd build_vs2017_win64
+FOR /F "delims=" %%i IN ('dir "C:\Program Files (x86)\Windows Kits\10\Include" /b /ad-h /t:c /o-n') DO (
+    SET windows_sdk_version=%%i
+    GOTO :found
+)
+call :colorEcho %red% No Windows SDK found in location: C:\Program Files (x86)\Windows Kits\10\Include
+EXIT 1
+:found
+SET windows_sdk_version=%CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION%
+echo Latest installed Windows SDK: %windows_sdk_version%
+echo Windows SDK required: 10.0.17763.0 or newer
 
-echo cmake -DCMAKE_SYSTEM_VERSION=%windows_sdk_version% -G "Visual Studio 16 2019" -A x64 ..
-cmake -DCMAKE_SYSTEM_VERSION=%windows_sdk_version% -G "Visual Studio 16 2019" -A x64 ..
+mkdir vsproject
+cd vsproject
+
+echo cmake -G "Visual Studio 16 2019" -A x64 ..
+cmake -G "Visual Studio 16 2019" -A x64 ..
 
 if errorlevel 1 (
     echo ==== CMake finished with errors ====
